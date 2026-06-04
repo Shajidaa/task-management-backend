eb18fa849dbbeef4797b0ca6a3bd73555264819a
Shajidaa <shajidaakterlopa101@gmail.com>
2026-06-04 23:32:05 +0600

    project conntact with neno console

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express, {
+  type Application,
+  type Request,
+  type Response,
+} from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ status: "Task management API is running smoothly" });
+});
+
+export default app;
